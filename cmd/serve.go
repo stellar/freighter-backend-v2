6ad60cf3695b33687283f42c1d4926f5b19f48de
Aditya Vyas <aditya.vyas@stellar.org>
2025-04-01 11:15:56 -0400

    remove token prices config

--- cmd/serve.go
+++ cmd/serve.go
@@ -48,15 +48,6 @@ func (s *serveCmd) Command() *cobra.Command {
 	cmd.Flags().IntVar(&s.cfg.RedisConfig.Port, "redis-port", 6379, "The Redis port")
 	cmd.Flags().StringVar(&s.cfg.RedisConfig.Password, "redis-password", "", "Redis password")
 
-	// Prices Config
-	cmd.Flags().BoolVar(&s.cfg.PricesConfig.DisableTokenPrices, "disable-token-prices", false, "Disable token prices")
-	cmd.Flags().StringVar(&s.cfg.PricesConfig.HorizonURL, "token-prices-horizon-url", "https://horizon.stellar.org", "The URL of the Horizon")
-	cmd.Flags().IntVar(&s.cfg.PricesConfig.BatchUpdateDelayMilliseconds, "token-prices-batch-update-delay", 5000, "Delay between batch updates in milliseconds")
-	cmd.Flags().IntVar(&s.cfg.PricesConfig.CalculationTimeoutMilliseconds, "token-prices-calculation-timeout", 10000, "Timeout for price calculations in milliseconds")
-	cmd.Flags().IntVar(&s.cfg.PricesConfig.UpdateIntervalMilliseconds, "token-prices-update-interval", 30000, "Interval between price updates in milliseconds")
-	cmd.Flags().IntVar(&s.cfg.PricesConfig.UpdateBatchSize, "token-prices-update-batch-size", 50, "Size of price update batches")
-	cmd.Flags().IntVar(&s.cfg.PricesConfig.StalenessThreshold, "token-prices-staleness-threshold", 300000, "Threshold for price staleness")
-
 	// Blockaid Config
 	cmd.Flags().StringVar(&s.cfg.BlockaidConfig.BlockaidAPIKey, "blockaid-api-key", "", "Blockaid API key")
 	cmd.Flags().BoolVar(&s.cfg.BlockaidConfig.UseBlockaidDappScanning, "use-blockaid-dapp-scanning", false, "Enable Blockaid dapp scanning")
@@ -72,7 +63,6 @@ func (s *serveCmd) Command() *cobra.Command {
 }
 
 func (s *serveCmd) Run() error {
-	logger.Info("Starting API server")
 	server := api.NewApiServer(s.cfg)
 	return server.Start()
 }
