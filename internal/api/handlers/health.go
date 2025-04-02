245b5f158642ceac37c1e49de210aa84ee448942
Aditya Vyas <aditya.vyas@stellar.org>
2025-04-02 17:19:32 -0400

    change header for health check

--- internal/api/handlers/health.go
+++ internal/api/handlers/health.go
@@ -7,9 +7,10 @@ import (
 )
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("OK"))
 	if err != nil {
-		logger.Error("Error writing response: %v", err)
+		logger.Error("Error writing health check response", "error", err)
 	}
 }
--- internal/api/middleware/header.go
+++ internal/api/middleware/header.go
@@ -7,7 +7,8 @@ import (
 func ResponseHeader() Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.Header().Set("X-Frame-Options", "DENY")
 			next.ServeHTTP(w, r)
 		})
 	}
