d221e35074186013dcbef9828932dfa6c765ca98
Aditya Vyas <aditya.vyas@stellar.org>
2025-12-12 10:15:54 -0500

    move mocks to single file

--- internal/utils/mocks.go
+++ internal/utils/mocks.go
@@ -3,10 +3,10 @@ package utils
 import (
 	"context"
 
-	"github.com/stellar/freighter-backend-v2/internal/types"
 	"github.com/stellar/go/txnbuild"
 	"github.com/stellar/go/xdr"
 	"github.com/stellar/stellar-rpc/client"
+	"github.com/stellar/freighter-backend-v2/internal/types"
 )
 
 type MockRPCService struct {
@@ -88,5 +88,30 @@ func (m *MockRPCService) GetLedgerEntries(ctx context.Context, keys []string, ne
 	if m.GetLedgerEntryError != nil {
 		return nil, m.GetLedgerEntryError
 	}
+	return nil, nil
+}
+
+type MockWalletBackendService struct {
+	GetBalancesOverride interface{}
+	GetBalancesError    error
+}
+
+func (m *MockWalletBackendService) Name() string {
+	return "mock-wallet-backend"
+}
+
+func (m *MockWalletBackendService) GetHealth(ctx context.Context, network string) (types.GetHealthResponse, error) {
+	return types.GetHealthResponse{Status: types.StatusHealthy}, nil
+}
+
+func (m *MockWalletBackendService) GetBalancesByAccountAddresses(ctx context.Context, addresses []string, network string) (interface{}, error) {
+	if m.GetBalancesError != nil {
+		return nil, m.GetBalancesError
+	}
+
+	if m.GetBalancesOverride != nil {
+		return m.GetBalancesOverride, nil
+	}
+
 	return nil, nil
 }
